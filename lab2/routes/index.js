84d0c667170495a9184221a487185e39973b9cec
Simarpreet Singh <200413865@student.georgianc.on.ca>
2020-06-04 23:09:31 -0400

    first commit

--- lab2/routes/index.js
+++ lab2/routes/index.js
@@ -4,6 +4,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
+    //console.log(req.query)
+    var method = req.query.method;
+    var x = req.query.x;
+    var y = req.query.y;
+    console.log(method + " " + x + " " + y);
+    var result = 0;
+    if (method == "add") {
+        result = x + " + " + y + " = " + (parseInt(x) + parseInt(y));
+    }
+    else if (method == "subtract") {
+        result = x + " - " + y + " = " + (parseInt(x) - parseInt(y));
+    }
+    else if (method == "multiply") {
+        result = x + " * " + y + " = " + (parseInt(x) * parseInt(y));
+    }
+    else if (method == "divide") {
+        result = x + " / " + y + " = " + (parseInt(x) / parseInt(y));
+    }
+    else {
+        result = "Please enter valid format";
+    }
+    console.log(result);
     res.render('index', { title: 'Express' });
 });
 
